27cac4a54d7113d21ed7039255f307ce896be19f
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-11-06 18:41:12 +0700

    app(header): add header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  template: ` <header>this is header</header> `,
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
+  templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  faBars = faBars;
+  faX = faX;
+
+  isOpen: boolean = false;
+
+  ngOnInit() {
+    this.menuClick(this.isOpen);
+  }
+
+  menuClick(b: boolean) {
+    this.isOpen = b;
+  }
+}
