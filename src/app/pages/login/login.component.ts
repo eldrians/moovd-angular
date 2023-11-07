a61103b05447f4bc1edda48e370491cff9459042
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-11-08 03:25:57 +0700

    app(login): create a login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,59 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
 
+  constructor(
+    private fb: FormBuilder,
+    private authServices: AuthService,
+    private router: Router
+  ) {}
+
+  get email() {
+    return this.loginForm.controls['email'];
+  }
+
+  get password() {
+    return this.loginForm.controls['password'];
+  }
+
+  loginUser() {
+    const { email, password } = this.loginForm.value;
+    this.authServices.getUserByEmail(email as string).subscribe(
+      (response) => {
+        if (response.length > 0 && response[0].password === password) {
+          sessionStorage.setItem('email', email as string);
+          this.router.navigate(['/gps']);
+        }
+        // else {
+        //   this.msgService.add({
+        //     severity: 'error',
+        //     summary: 'Error',
+        //     detail: 'email or password is wrong',
+        //   });
+        // }
+      },
+      (error) => {
+        console.log(error);
+        // this.msgService.add({
+        //   severity: 'error',
+        //   summary: 'Error',
+        //   detail: 'Something went wrong',
+        // });
+      }
+    );
+  }
 }
