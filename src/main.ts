96df1b58a8adec0c1678622a08f0df9f28fe1f1d
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-11-09 00:22:04 +0700

    app(): add service worker

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,22 @@
 
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import {
   provideHttpClient,
   withInterceptorsFromDi,
 } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { AppModule } from './app/app.module';
 import { GpsListComponent } from './app/pages/gps/gps-list/gps-list.component';
 import { GpsDetailComponent } from './app/pages/gps/gps-detail/gps-detail.component';
 import { LoginComponent } from './app/pages/login/login.component';
 import { RegisterComponent } from './app/pages/register/register.component';
 import { authGuard } from './app/core/guard/auth.guard';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(BrowserModule, AppModule),
+    importProvidersFrom(BrowserModule),
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter([
       {
@@ -41,5 +41,9 @@ bootstrapApplication(AppComponent, {
       { path: '', redirectTo: 'gps', pathMatch: 'full' },
       { path: '**', redirectTo: 'gps', pathMatch: 'full' },
     ]),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 }).catch((err) => console.error(err));
