1326ce781a284521446f6ba0c77e39296d027960
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-11-09 04:28:47 +0700

    app(AppComponent): add ngContainer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,25 +8,28 @@ import { HeaderComponent, SidebarComponent } from './shared/components';
   selector: 'app-root',
   standalone: true,
   template: `
-    <div class="w-full flex flex-col lg:flex-row" *ngIf="isMainRoute()">
-      <div class="hidden lg:block lg:w-1/5">
-        <app-sidebar class="hidden lg:block lg:w-1/5 lg:fixed"></app-sidebar>
-      </div>
-      <div class="w-full lg:w-4/5">
-        <div class="w-full lg:w-4/5 h-[50px]">
-          <app-header class="w-full lg:w-4/5 h-[50px] fixed z-50"></app-header>
+    <ng-container *ngIf="isMainRoute()">
+      <div class="w-full flex flex-col lg:flex-row">
+        <div class="hidden lg:block lg:w-1/5">
+          <app-sidebar class="hidden lg:block lg:w-1/5 lg:fixed"></app-sidebar>
         </div>
-        <router-outlet class="z-10"></router-outlet>
+        <div class="w-full lg:w-4/5">
+          <div class="w-full lg:w-4/5 h-[50px]">
+            <app-header
+              class="w-full lg:w-4/5 h-[50px] fixed z-50"
+            ></app-header>
+          </div>
+          <router-outlet class="z-10"></router-outlet>
+        </div>
       </div>
-    </div>
-    <div
-      class="w-full flex justify-center items-center"
-      *ngIf="isLoginOrRegisterRoute()"
-    >
-      <div class="w-full h-screen">
-        <router-outlet class="z-10"></router-outlet>
+    </ng-container>
+    <ng-container *ngIf="isLoginOrRegisterRoute()">
+      <div class="w-full flex justify-center items-center">
+        <div class="w-full h-screen">
+          <router-outlet class="z-10"></router-outlet>
+        </div>
       </div>
-    </div>
+    </ng-container>
   `,
   imports: [CommonModule, SidebarComponent, HeaderComponent, RouterModule],
 })
